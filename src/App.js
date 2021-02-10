fb385966b4312475ba83230b02ccdae606447d96
ChaseReineke <chase.e.reineke@gmail.com>
2021-02-09 22:38:57 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Tabs from './components/MainComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <Tabs />
+    </div>
+  );
+}
+
+export default App;
